6b82204f0c558648d82f55501e549a3eea3d99a3
Soma Sai Kiran Reddy Chinnam <saikira@xilinx.com>
2022-09-20 10:05:36 +0530

    Create options.cmake

---
+++ cmake/options.cmake
@@ -0,0 +1,6 @@
+#########################################################################
+# Copyright (c) 2021, Xilinx Inc. and Contributors. All rights reserved.
+# SPDX-License-Identifier: MIT
+#########################################################################
+
+#Placeholder file
