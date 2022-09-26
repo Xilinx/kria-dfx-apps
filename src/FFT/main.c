f58fc74acac3badeb5b434982af85f71bd704893
Soma Sai Kiran Reddy Chinnam <saikira@xilinx.com>
2022-09-26 14:30:37 +0530

    Add files via upload

--- src/FFT/main.c
+++ src/FFT/main.c
@@ -16,7 +16,24 @@
 #include "experimental/xrt_device.h"
 #include "experimental/xrt_kernel.h"
 
-#define SIZE_IN_BYTES       0x40000
+#define SIZE_IN_BYTES       0x4000000	//64MB
+#define ONE_KB				1024		//Number of Bytes
+#define SIXTEEN_KB			16384		//Number of Bytes
+
+#define CONFIG_OFFSET		0		//Config Buffer Offset
+#define FFT_RESULT 			17408	//Result of FFT is stored at offset 0x11000 (69632d = 17408*4) as address increments by 32bit or 4 bytes at a time
+#define CONFIG_OFFSET_MEM	0x0 	//Config Mem Offset in Hex
+#define CONFIG_SIZE			0x1		//Size of Config Buffer in multiples of 16 bytes. (1d*16 bytes = 16 bytes)
+#define TID_0				0x0		//TID 0
+#define TID_1				0x1		//TID 1
+#define TID_2				0x2		//TID 2
+#define TID_3				0x3		//TID 3
+#define TID_4				0x4		//TID 4
+#define FFT_DATA_IN_OFFSET_MEM	0x1000 	//Config Mem Offset in Hex
+#define RESULT_OFFSET_MEM	0x11000	//Result Buffer Mem Offset in Hex
+#define FFT_DATA_IN_SIZE	0x1000	//Size of Config Buffer in multiples of 16 bytes. (4096d*16 bytes = 65536 bytes)
+#define RESULT_SIZE			0x1000	//Result Buffer Mem Offset in Hex
+
 
 int InitializeMapRMs(int slot);
 int FinaliseUnmapRMs(int slot);
@@ -25,17 +42,21 @@ int DataToAccel(int slot, uint64_t data, uint64_t size, uint8_t tid);
 int DataFromAccel(int slot, uint64_t data, uint64_t size);
 int DataToAccelDone(int slot);
 int DataFromAccelDone(int slot);
-//char * dfxmgr_uio_by_name(char *obuf, int slot, const char *name);
 
+//Input to config port of FFT IP
 uint32_t config[] = {0x0000000c,0x0000000c,0x0000000c,0x0000000c};
 
+//FFT Input Data
 uint32_t fft_data_in[] = {
 	0xcca5a729, 0x4b276e90, 0x9a57a7e7, 0xd0bfe1c7
 };
+
+//FFT Reference output Data
 uint32_t fft_data_out[] = {
 	0x88009C00, 0x5C006800, 0x5800E400, 0x1400A800
 };
 
+//Placeholder buffer for actual output from FFT
 uint32_t resultbuff[] = {
 	0x00000000, 0x00000000, 0x00000000, 0x00000000,
 	0x00000000, 0x00000000, 0x00000000, 0x00000000,
@@ -43,11 +64,12 @@ uint32_t resultbuff[] = {
 	0x00000000, 0x00000000, 0x00000000, 0x00000000
 };
 
+//Tests if FFT test has passed by comparing the result buffer pointer by 'vptr' against the reference fir_data_out.
 void isTestPassed(uint32_t *vptr)
 {
 	for (int i=0; i < 16; i++)					//Copying out data to resultbuff for comparison with golden data
 	{
-		resultbuff[i] = vptr[(i*1024)+17408];
+		resultbuff[i] = vptr[(i*ONE_KB)+FFT_RESULT];
 	}
 	int same_flag = 1;
 		for (int i=0; i< 16; i++)
@@ -59,31 +81,49 @@ void isTestPassed(uint32_t *vptr)
 			}
 		}
 		if(same_flag)
-			printf("\t === TEST PASSED ===\n");
+			printf("\t Success: FFT TEST PASSED !\n");
 		else
-			printf("\t === TEST FAILED ===\n");
+			printf("\t Error: FFT TEST FAILED !\n");
 }
 		
 int main(int argc, char *argv[])
 {
 	int slot =0;
 	if(argc>1)
+	{
+		//Updating slot number provided as command line argument
 		slot = atoi (argv[1]);
+		if (slot != 1 && slot != 0)
+		{
+			printf("- Invalid slot number provided %s. Valid values : 0 or 1\n",argv[1]);
+			return 0;
+		}
+	}
+	
+	//Initialize and memory map RMs
+	if(InitializeMapRMs(slot) == -1)
+	{
+		printf("- Check the slot number where the accelerator is loaded and run the test on the specific slot.\n");
+		return 0;
+	}
 
-	InitializeMapRMs(slot);
+	
+	//Allocate XRT buffer to be used for input and output of application
 	auto device = xrt::device(0);
-	auto bo_a = xrt::bo(device, SIZE_IN_BYTES, 0);
-	uint32_t *vptr = (uint32_t *)bo_a.map<int*>();
-	mapBuffer(bo_a);
+	auto bufferObject = xrt::bo(device, SIZE_IN_BYTES, 0);
+	uint32_t *vptr = (uint32_t *)bufferObject.map<int*>();
+	mapBuffer(bufferObject);
 
-    	std::memcpy(vptr, &config, sizeof(config));
-   	for (int i=0;i<16384;i=i+4)
-	    std::memcpy(vptr+1024+i, &fft_data_in, sizeof(fft_data_in));
+	//Write to the memory that was mapped, use devmem from the command line of Linux to verify it worked
+	//Write Config Buffer to memory at CONFIG_OFFSET of allocated XRT Buffer
+    std::memcpy(vptr+CONFIG_OFFSET, &config, sizeof(config));
+   	for (int i=0;i<SIXTEEN_KB;i=i+4)
+	    std::memcpy(vptr+ONE_KB+i, &fft_data_in, sizeof(fft_data_in));
     
 	printf("FFT TEST :\n");
 	//Configure FFT Ch0
 	//Arguments: (int slot, uint64_t offset, uint64_t size, uint8_t tid)
-	DataToAccel(slot,0x0,0x1,0x1);
+	DataToAccel(slot,CONFIG_OFFSET_MEM,CONFIG_SIZE,TID_1);
 	int status = DataToAccelDone(slot);
 
 	//Configure FFT Ch1
@@ -91,7 +131,7 @@ int main(int argc, char *argv[])
 	if(status)
 	{
 		printf("\t Configure FFT Ch0 done.\n");
-		DataToAccel(slot,0x0,0x1,0x2);
+		DataToAccel(slot,CONFIG_OFFSET_MEM,CONFIG_SIZE,TID_2);
 		status = DataToAccelDone(slot);
 	}
 	
@@ -100,7 +140,7 @@ int main(int argc, char *argv[])
 	if(status)
 	{
 		printf("\t Configure FFT Ch1 done.\n");
-		DataToAccel(slot,0x0,0x1,0x3);
+		DataToAccel(slot,CONFIG_OFFSET_MEM,CONFIG_SIZE,TID_3);
 		status = DataToAccelDone(slot);
 	}
 	//Configure FFT Ch3
@@ -108,19 +148,18 @@ int main(int argc, char *argv[])
 	if(status)
 	{
 		printf("\t Configure FFT Ch2 done.\n");
-		DataToAccel(slot,0x0,0x1,0x4);
+		DataToAccel(slot,CONFIG_OFFSET_MEM,CONFIG_SIZE,TID_4);
 		status = DataToAccelDone(slot);
 	}
-	//Config MM2S data
-	//Arguments: (int slot, uint64_t offset, uint64_t size, uint8_t tid)
+	//Config FFT Data In
 	if(status)
 	{
 		printf("\t Configure FFT Ch3 done.\n");
-		DataToAccel(slot,0x1000,0x1000,0x0);
+		DataToAccel(slot,FFT_DATA_IN_OFFSET_MEM,FFT_DATA_IN_SIZE,TID_0);
  	//Config S2MM data
 	//Arguments: (int slot, uint64_t offset, uint64_t size)
 		printf("\t Configure FFT data done.\n");
-		DataFromAccel(slot, 0x11000, 0x1000);
+		DataFromAccel(slot, RESULT_OFFSET_MEM, RESULT_SIZE);
 		status = DataFromAccelDone(slot);
 	}
   
@@ -132,7 +171,7 @@ int main(int argc, char *argv[])
 		isTestPassed(vptr);
 		FinaliseUnmapRMs(slot);
 	}
-	printf("FFT TEST done.\n");
+
 	return 0;
 
 }
