74081f5d5e6b1cbf119e00554e4c6124051ba444
Sergei Korneichuk <sergei.korneichuk@amd.com>
2022-09-28 19:28:38 -0700

    cmake: add code for Ubuntu native build
    Use simple cmake project to build Kria DFX demos
    
    Signed-off-by: Sergei Korneichuk <sergei.korneichuk@amd.com>

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,6 +1,21 @@
 #########################################################################
-# Copyright (c) 2021, Xilinx Inc. and Contributors. All rights reserved.
+# Copyright (c) 2022, Xilinx Inc. and Contributors. All rights reserved.
 # SPDX-License-Identifier: MIT
 #########################################################################
 
-#Placeholder file
+if (NOT DEFINED CMAKE_BUILD_TYPE)
+  set (CMAKE_BUILD_TYPE Debug)
+endif(NOT DEFINED CMAKE_BUILD_TYPE)
+message ("-- Build type: ${CMAKE_BUILD_TYPE}")
+
+find_path (XRT_INCD     NAMES xclbin.h        PATH_SUFFIXES xrt     REQUIRED)
+find_path (DFX_MGR_INCD NAMES dfxmgr_client.h PATH_SUFFIXES dfx-mgr REQUIRED)
+include_directories (${XRT_INCD} ${DFX_MGR_INCD})
+
+find_library (XRT_LIBRARIES NAMES xrt_coreutil REQUIRED)
+find_library (XRT_LIBXX NAMES xrt++        REQUIRED)
+find_library (DFXMGR_LIB NAMES dfx-mgr     REQUIRED)
+
+message ("-- XRT_LIBRARIES : ${XRT_LIBRARIES}")
+message ("-- XRT_LIBXX : ${XRT_LIBXX}")
+message ("-- DFXMGR_LIB : ${DFXMGR_LIB}")
