39781f5de41d6989b7924d23a4badb3b4951eed1
Santa Kancherla <santa.kancherla@xilinx.com>
2022-11-02 15:12:12 +0530

    Added feature on FFT: file input

--- src/FFT/main.c
+++ src/FFT/main.c
@@ -10,6 +10,8 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <getopt.h>
+#include <sys/stat.h>
 
 // XRT includes
 #include "experimental/xrt_bo.h"
@@ -21,19 +23,28 @@
 #define SIXTEEN_KB			16384		//Number of Bytes
 
 #define CONFIG_OFFSET		0		//Config Buffer Offset
-#define FFT_RESULT 			17408	//Result of FFT is stored at offset 0x11000 (69632d = 17408*4) as address increments by 32bit or 4 bytes at a time
 #define CONFIG_OFFSET_MEM	0x0 	//Config Mem Offset in Hex
 #define CONFIG_SIZE			0x1		//Size of Config Buffer in multiples of 16 bytes. (1d*16 bytes = 16 bytes)
+
+#define FFT_DATA_IN_OFFSET_MEM	0x1000 	//Config Mem Offset in Hex
+#define FFT_DATA_IN_SIZE	0x1000	//Size of Config Buffer in multiples of 16 bytes. (4096d*16 bytes = 65536 bytes)
+
+#define FFT_RESULT 			17408	//Result of FFT is stored at offset 0x11000 (69632d = 17408*4) as address increments by 32bit or 4 bytes at a time
+#define RESULT_OFFSET_MEM	0x11000	//Result Buffer Mem Offset in Hex
+#define RESULT_SIZE			0x1000	//Result Buffer Mem Offset in Hex
+
 #define TID_0				0x0		//TID 0
 #define TID_1				0x1		//TID 1
 #define TID_2				0x2		//TID 2
 #define TID_3				0x3		//TID 3
 #define TID_4				0x4		//TID 4
-#define FFT_DATA_IN_OFFSET_MEM	0x1000 	//Config Mem Offset in Hex
-#define RESULT_OFFSET_MEM	0x11000	//Result Buffer Mem Offset in Hex
-#define FFT_DATA_IN_SIZE	0x1000	//Size of Config Buffer in multiples of 16 bytes. (4096d*16 bytes = 65536 bytes)
-#define RESULT_SIZE			0x1000	//Result Buffer Mem Offset in Hex
 
+
+#define die(fmt, args ...) do { fprintf(stderr, \
+	    "ERROR:%s():%u " fmt ": %s\n", \
+	    __func__, __LINE__, ##args, errno ? strerror(errno) : ""); \
+	    exit(EXIT_FAILURE); \
+	} while (0)
 
 int InitializeMapRMs(int slot);
 int FinaliseUnmapRMs(int slot);
@@ -86,19 +97,11 @@ void isTestPassed(uint32_t *vptr)
 			printf("\t Error: FFT TEST FAILED !\n");
 }
 		
-int main(int argc, char *argv[])
+int 
+internal_test(int slot)
 {
-	int slot =0;
-	if(argc>1)
-	{
-		//Updating slot number provided as command line argument
-		slot = atoi (argv[1]);
-		if (slot != 1 && slot != 0)
-		{
-			printf("- Invalid slot number provided %s. Valid values : 0 or 1\n",argv[1]);
-			return 0;
-		}
-	}
+	if (slot != 1 && slot != 0)
+		die("Invalid slot: %d - must be 0 or 1", slot);
 	
 	//Initialize and memory map RMs
 	if(InitializeMapRMs(slot) == -1)
@@ -107,7 +110,6 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	
 	//Allocate XRT buffer to be used for input and output of application
 	auto device = xrt::device(0);
 	auto bufferObject = xrt::bo(device, SIZE_IN_BYTES, 0);
@@ -174,4 +176,149 @@ int main(int argc, char *argv[])
 
 	return 0;
 
+}
+
+static struct option const long_opt[] =
+{
+	{ "help",             no_argument, NULL, 'h'},
+	{ "config",        required_argument, NULL, 'c'},
+	{ "out",        required_argument, NULL, 'o'},
+	{ "slot",       required_argument, NULL, 's'},
+	{ NULL, 0, NULL, 0}
+};
+
+static const char help_usage[] =
+  "Usage: FFT_PROG [<options>] --slot rm_slot --config filename --out out_file in_file\n"
+  " FFT_PROG (0|1) preform a quick internal test for slot 0 or 1\n\n"  
+  "Options are:\n"
+  "  -h, --help\n"
+  "  -s, --slot rm_slot    Set slot to rm_slot: 0 or 1. Default 0\n"
+  "  -o, --out out_file    Write output to file\n"
+  "  -c, --config filename Use config file\n";
+
+void
+usage(const char *msg)
+{
+	fprintf(stderr, "%s\n%s", msg, help_usage);
+	exit(0);
+}
+
+int
+main(int argc, char *argv[])
+{
+	char *config_file = NULL;
+	char *in_file  = NULL;
+	char *out_file = NULL;
+	struct stat statbuf;
+    struct stat configbuf;
+	int slot = 0, infd = -1, outfd = -1, configfd = -1;
+	int rc, opt;
+
+	if (argc == 1)
+		return internal_test(0);
+	else if (argc == 2 && argv[1][0] != '-')
+		return internal_test(argv[1][0] - '0');
+
+	while ((opt = getopt_long(argc, argv, "hc:o:s:",
+				  long_opt, NULL)) != -1)
+		switch (opt) {
+		case 'c':
+			config_file = optarg;
+			break;
+		case 'o':
+			out_file = optarg;
+			break;
+		case 's':
+			slot = atoi(optarg);
+			break;
+		case 'h':
+		default:
+			usage("getopt");
+		}
+
+	if (!config_file || !out_file)
+		die("missing config and/or output file");
+
+	if (optind >= argc)
+		die("Expected filename after options");
+	
+	in_file = argv[argc-1];
+	infd = open(in_file, O_RDONLY, 0);
+	if (infd == -1)
+		die("open(%s)", in_file);
+	if (fstat(infd, &statbuf))
+		die("fstat(%s)", in_file);
+	size_t in_len = statbuf.st_size;
+	char *in_mm = (char *)mmap(NULL, in_len, PROT_READ, MAP_SHARED, infd, 0);
+
+	configfd = open(config_file, O_RDONLY, 0);
+	if (configfd == -1)
+		die("open(%s)", config_file);
+    if (fstat(configfd, &configbuf))
+		die("fstat(%s)", config_file);
+    size_t config_len = configbuf.st_size;
+	char *config_mm = (char *)mmap(NULL, config_len, PROT_READ, MAP_SHARED, configfd, 0);
+
+	outfd = open(out_file, O_WRONLY | O_CREAT | O_TRUNC,
+		     S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (outfd == -1)
+		die("open(%s)", out_file);
+
+	printf("in_file, sz, out_file = %s, %ld %s\n",
+		in_file, statbuf.st_size, out_file);
+
+	//Initialize and memory map RMs
+	if(InitializeMapRMs(slot) == -1)
+		die("Use --slot option in this test; see xmutil listapps");
+
+	//Allocate XRT buffer to be used for input and output of application
+	auto device = xrt::device(0);
+	auto bufferObject = xrt::bo(device, SIZE_IN_BYTES, 0);
+	uint32_t *vptr = (uint32_t *)bufferObject.map<int*>();
+	uint64_t uvp = (uint64_t)vptr;
+	mapBuffer(bufferObject);
+
+	int status;
+	// Write to the memory that was mapped, use devmem from
+	// the command line of Linux to verify it worked
+
+	// Write the config and the data
+	memcpy(vptr + CONFIG_OFFSET, config_mm, config_len);
+	memcpy(vptr +  ONE_KB, in_mm, in_len);
+
+	// Configure four FFT channels
+	for (int i=1; i<=4; i++)
+	{
+		//Arguments: (int slot, uint64_t offset, uint64_t size, uint8_t tid)
+		DataToAccel(slot, CONFIG_OFFSET_MEM, config_len>>4, i);
+		status = DataToAccelDone(slot);
+		if (!status)
+			die("DataToAccelDone(%d)", slot);
+		printf("\t Configure FFT Ch%d done.\n", i);
+	}
+	
+	//Config FFT Data In
+ 	//Config S2MM data
+	//Arguments: (int slot, uint64_t offset, uint64_t size)
+	DataToAccel(slot, FFT_DATA_IN_OFFSET_MEM, in_len>>4, TID_0);
+	if (!status)
+		die("DataToAccelDone(%d)", slot);
+	printf("\t Configure FFT data done.\n");
+
+	DataFromAccel(slot, RESULT_OFFSET_MEM, in_len>>4);
+	status = DataFromAccelDone(slot);
+	if (!status)
+		die("DataFromAccelDone(%d)", slot);
+	printf("\t Data from Accel done.\n");
+
+	const void *cvp = (const void *)(uvp + RESULT_OFFSET_MEM);
+	// write only the original size
+	status = write(outfd, cvp, statbuf.st_size);
+	FinaliseUnmapRMs(slot);
+	close(infd);
+	close(outfd);
+	if (!status)
+		die("write(%s)", out_file);
+
+	return 0;
 }
