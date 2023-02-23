f20c725cf4f03a978267aa4d6c27b5a453259062
Santa Kancherla <santa.kancherla@xilinx.com>
2023-02-23 16:33:04 +0530

    Update main.c

--- src/FIR/main.c
+++ src/FIR/main.c
@@ -34,7 +34,7 @@
 #define RELOAD_OFFSET_MEM	0x0 	//Reload Mem Offset in Hex
 #define RELOAD_SIZE			0xA		//Size of Reload Buffer in multiples of 16 bytes. (10d*16 bytes = 160 bytes)
 
-#define RESULT_OFFSET_MEM	0x400	//Result Buffer Mem Offset in Hex
+#define RESULT_OFFSET_MEM	0x11000	//Result Buffer Mem Offset in Hex
 #define RESULT_SIZE			0x1000	//Result Buffer Mem Offset in Hex
 
 #define TID_0				0x0		//TID 0
